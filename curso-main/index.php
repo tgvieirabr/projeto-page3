d96440252c0e918d02eb42c5d6a51ce846582bb7
tgvieirabr <tgvieirabr@gmail.com>
2021-08-17 06:22:49 -0300

    tada

---
+++ curso-main/index.php
@@ -0,0 +1,109 @@
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8" />
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport">
+    <title>Home</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+    <link rel="stylesheet" href="./assets/libs/bxslider/css/jquery.bxslider.css?v=1">
+    <script src="./assets/libs/bxslider/js/jquery.bxslider.js"></script>
+    <link href="./assets/css/styles.css?v=2" rel="stylesheet" />
+</head>
+
+<body>
+    <!-- CABEÇALHO -->
+    <header>
+        <div class="container">
+            <img src="./assets/img/logo.png" id="logo" />
+
+            <nav class="menu">
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">Sobre</a>
+                    </li>
+                    <li>
+                        <a href="#">Serviços</a>
+                    </li>
+                    <li>
+                        <a href="#">Contato</a>
+                    </li>
+                </ul>
+            </nav>
+
+        </div>
+    </header>
+    <!-- FIM CABEÇALHO -->
+    <section id="banners">
+        <div class="slider">
+
+            <div>
+
+                <img src="./assets/img/banner.png" />
+
+                <div class="banner-descricao">
+
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-12 col-md-10">
+                                <h2>
+                                    Lorem ipsum dolor sit amet,
+                                    consectetur adipiscing elit.
+                                </h2>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                </p>
+                                <a href="">Saiba mais</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div>
+                <img src="./assets/img/banner.png" />
+
+
+                <div class="banner-descricao">
+
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-12 col-md-10">
+                                <h2>
+                                    Lorem ipsum dolor sit amet,
+                                    consectetur adipiscing elit.
+                                </h2>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                </p>
+                                <a href="">Saiba mais</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="./assets/js/scripts.js?v=4"></script>
+
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,253 @@
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8" />
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport">
+    <title>Home</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+    <link rel="stylesheet" href="./assets/libs/bxslider/css/jquery.bxslider.css?v=1">
+    <script src="./assets/libs/bxslider/js/jquery.bxslider.js"></script>
+    <link href="./assets/css/styles.css?v=2" rel="stylesheet" />
+</head>
+
+<body>
+    <!-- CABEÇALHO -->
+    <header>
+        <div class="container">
+            <img src="./assets/img/logo.png" id="logo" />
+
+            <nav class="menu">
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">Sobre</a>
+                    </li>
+                    <li>
+                        <a href="#">Serviços</a>
+                    </li>
+                    <li>
+                        <a href="#">Contato</a>
+                    </li>
+                </ul>
+            </nav>
+
+        </div>
+    </header>
+    <!-- FIM CABEÇALHO -->
+    <section id="banners">
+        <div class="slider">
+
+            <div>
+
+                <img src="./assets/img/banner.png" />
+
+                <div class="banner-descricao">
+
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-12  col-sm-12 col-md-12">
+                                <h2>
+                                    Lorem ipsum dolor sit amet,
+                                    consectetur adipiscing elit.
+                                </h2>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                </p>
+                                <a class="btn" href="">Saiba mais</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div>
+                <img src="./assets/img/banner.png" />
+
+
+                <div class="banner-descricao">
+
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-12 col-md-10">
+                                <h2>
+                                    Lorem ipsum dolor sit amet,
+                                    consectetur adipiscing elit.
+                                </h2>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                </p>
+                                <a class="btn" href="">Saiba mais</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+    <section id="sobre">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h3 class="titulo">Sobre</h3>
+                    <h4 class="subtitulo mb-5">Conheça mais</h4>
+
+                </div>
+
+
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a porta eros, et dictum arcu. Pellentesque sit amet fermentum lacus. Proin tempus nulla vitae risus faucibus hendrerit. Donec euismod tincidunt condimentum. Praesent ac ligula in erat venenatis vestibulum. Fusce ligula dui, vulputate sed sapien ac, interdum placerat orci. Nunc gravida tellus quis mi posuere gravida nec quis turpis. Vivamus egestas nec massa nec dapibus. Duis mollis cursus mauris at hendrerit. Nulla vitae erat mauris. Ut pulvinar ultricies leo, id euismod leo accumsan et. Phasellus consectetur dignissim ipsum, sed egestas tortor suscipit eu. Cras pellentesque odio ut magna interdum, quis mollis mauris pellentesque.
+
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a porta eros, et dictum arcu. Pellentesque sit amet fermentum lacus. Proin tempus nulla vitae risus faucibus hendrerit. Donec euismod tincidunt condimentum. Praesent ac ligula in erat venenatis vestibulum. Fusce ligula dui, vulputate sed sapien ac, interdum placerat orci. Nunc gravida tellus quis mi posuere gravida nec quis turpis. Vivamus egestas nec massa nec dapibus. Duis mollis cursus mauris. </p>
+
+                </div>
+                <div class="col-md-6">
+                    <img src="./assets/img/imgsobre.png" class="img-fluid" />
+                </div>
+
+            </div>
+
+        </div>
+
+    </section>
+    <section id=serviços>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h5 class="titulo">Serviços </h5>
+                    <h6 class="subtitulo mb-5">Como podemos ajudar </h6>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="./assets/img/imgsobre.png" class="img-fluid" />
+                    <h3>Titulo do serviço</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a porta eros, et dictum arcu. Pellentesque sit amet fermentum lacus. Proin tempus nulla vitae risus faucibus hendrerit. Donec euismod tincidunt condimentum. Praesent ac ligula in erat venenatis vestibulum.</p>
+                </div>
+                <div class="col-md-4">
+                    <img src="./assets/img/imgsobre.png" class="img-fluid" />
+                    <h3>Titulo do serviço</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a porta eros, et dictum arcu. Pellentesque sit amet fermentum lacus. Proin tempus nulla vitae risus faucibus hendrerit. Donec euismod tincidunt condimentum. Praesent ac ligula in erat venenatis vestibulum.</p>
+                </div>
+                <div class="col-md-4">
+                    <img src="./assets/img/imgsobre.png" class="img-fluid" />
+                    <h3>Titulo do serviço</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a porta eros, et dictum arcu. Pellentesque sit amet fermentum lacus. Proin tempus nulla vitae risus faucibus hendrerit. Donec euismod tincidunt condimentum. Praesent ac ligula in erat venenatis vestibulum.</p>
+                </div>
+            </div>
+        </div>
+
+
+
+    </section>
+    <section id="contato">
+        <h3 class="titulo">Contato</h3>
+        <h4 class="subtitulo">Mande sua Dúvida</h4>
+
+    </section>
+    <section id="contato">
+        <form>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-sm-5">
+
+                        <div class="form-group mb-3 ml-20">
+                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Digite seu email">
+                        </div>
+                        <div class="form-group mb-3">
+                            <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Digite seu Nome">
+
+                        </div>
+                        <div class="form-group mb-3">
+                            <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Assunto">
+
+
+                        </div>
+
+                    </div>
+                    <div class="col-sm-5">
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        <button class="btn">Enviar</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+
+    </section>
+    <section>
+        <!-- Footer -->
+        <footer class="rodape" class="bg-secondary text-white text-center text-md-start">
+            <!-- Grid container -->
+            <div class="container p-4">
+                <!--Grid row-->
+                <div class="row">
+                    <!--Grid column-->
+                    <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+                        <h5 class="text-uppercase">Endereço</h5> <br>
+
+                        <p>
+                            Nome da rua, nº 222 <br>
+                            Centro, Bento Gonçalves - Rio Grande do Sul <br>
+                            CEP: 95.700-000
+
+                        </p>
+                    </div>
+                    <!--Grid column-->
+
+                    <!--Grid column-->
+                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0 ">
+                        <h5 class="text-uppercase">Contato</h5>
+                        <br>
+                        Fone: (54) 9 9999-9999 <br>
+                        E-mail: contato@companyname.com
+                    </div>
+                    <!--Grid column-->
+
+                    <!--Grid column-->
+                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                        <h5 class="text-uppercase mb-0">Atendimento</h5>
+
+                        <br>
+                        Segunda a Sexta-feira <br>
+                        08:15 às 11:45 <br>
+                        13:15 às 18:30
+
+
+                    </div>
+                    <!--Grid column-->
+                </div>
+                <!--Grid row-->
+            </div>
+            <!-- Grid container -->
+
+            <!-- Copyright -->
+            <div class=" p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+                <a class="text-white" href="https://mdbootstrap.com/">Todos Direitos Reservados</a>
+            </div>
+            <!-- Copyright -->
+        </footer>
+        <!-- Footer -->
+    </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="./assets/js/scripts.js?v=4"></script>
+
+
+</body>
+
+</html>
